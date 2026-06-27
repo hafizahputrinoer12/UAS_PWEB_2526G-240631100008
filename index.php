f076013f665ad56d698ad543214b0a22b72eb1ea
hafizahputrinoer12 <hafizahputrinoer@gmail.com>
2026-06-27 08:41:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,53 @@
+<?php
+include 'koneksi.php';
+include 'functions.php';
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home - Sistem Pendataan Mahasiswa</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+
+    <h1>🎓 Sistem Pendataan Mahasiswa</h1>
+
+    <p>
+        Selamat datang di aplikasi Sistem Pendataan Mahasiswa.
+        Aplikasi ini digunakan untuk menambah, melihat, mengubah,
+        dan menghapus data mahasiswa.
+    </p>
+
+    <br>
+
+    <h3>Total Data Mahasiswa</h3>
+
+    <h1><?= jumlahData($conn); ?></h1>
+
+    <br>
+
+    <div class="menu">
+
+        <a class="btn-tambah" href="tambah.php">
+            ➕ Tambah Data
+        </a>
+
+        <a class="btn-lihat" href="daftar.php">
+            📋 Lihat Data
+        </a>
+
+    </div>
+
+    <br><br>
+
+    <div class="footer">
+        <p>© 2026 | Pemrograman Web | UAS Putri Noer Hafizah 240631100008 </p>
+    </div>
+
+</div>
+
+</body>
+</html>
