d294aabab0de287af4d850680a89c834485ad7b2
hafizahputrinoer12 <hafizahputrinoer@gmail.com>
2026-06-27 08:47:24 +0700

    Create simpan.php

---
+++ simpan.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'koneksi.php';
+
+$nim = $_POST['nim'];
+$nama = $_POST['nama'];
+$jurusan = $_POST['jurusan'];
+$alamat = $_POST['alamat'];
+
+mysqli_query($conn,"INSERT INTO mahasiswa
+VALUES('','$nim','$nama','$jurusan','$alamat')");
+
+header("Location: daftar.php");
+
+?>
