b3cbcd03d88586b30bfe28dfa83cc22cd3b62a6f
hafizahputrinoer12 <hafizahputrinoer@gmail.com>
2026-06-27 08:42:17 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,14 @@
+<?php
 
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "db_mahasiswa";
+
+$conn = mysqli_connect($host,$user,$pass,$db);
+
+if(!$conn){
+    die("Koneksi gagal");
+}
+
+?>
